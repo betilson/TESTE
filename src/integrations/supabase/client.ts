345bbe302b7b495dc236149b429916969cb4f8e2
[dyad] <albertochitata@gmail.com>
2025-09-07 10:38:50 +0100

    Reverted all changes back to version fedc0c75264ecc36646889b1c1879dd5074e97cf

--- src/integrations/supabase/client.ts
+++
@@ -1,6 +0,0 @@
-import * as SupabaseSsr from '@supabase/ssr'
-
-export const supabase = SupabaseSsr.createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
